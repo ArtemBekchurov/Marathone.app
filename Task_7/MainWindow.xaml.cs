02b9089254b5aa372d3b9d8e1204f277f266ccec
ArtemBekchurov <119742768+ArtemBekchurov@users.noreply.github.com>
2023-01-20 21:03:12 +0300

    Task_7

---
+++ Task_7/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Task_7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();   //Тимер
+
+
+        int num = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            num = num + 1;
+            lbl.Content = num.ToString();
+
+            if (num >= 30)
+            {
+                timer.Stop();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        
+    }
+}
